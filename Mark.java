2fb51b67132e92b3fa65514fe79288c9304e4306
Roland <roland.viss@mail.ru>
2013-08-29 17:59:21 +0300

    Create Mark.java

---
+++ Mark.java
@@ -0,0 +1,17 @@
+package ticTacToe;
+
+public enum Mark {
+
+	X("X"), O("O"), EMPTY("_");
+
+	public String value;
+
+	Mark(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+}
